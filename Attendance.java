07c6495ed8b8293dc51df29c29b685294b11af05
Pforpeetar <phamanpeter@gmail.com>
2016-05-04 23:21:34 -0700

    Models and Enums

---
+++ Attendance.java
@@ -0,0 +1,9 @@
+
+public class Attendance {
+	private int id;
+	private int location_id;
+	private int attendee_id;
+	private int date_id;
+	private int band_id;
+	private int attendance_count;
+}
---
+++ Date.java
@@ -0,0 +1,8 @@
+public class Date {
+	private int id;
+	private int day;
+	private DayOfTheWeek day_of_the_week;
+	private Month month;
+	private Quarter quarter;
+	private int year;
+}
